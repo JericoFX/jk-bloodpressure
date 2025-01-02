6c8c19daeec51ca8c24e3127aec211ddd514ff87
Johann Kraus <56100605+JericoFX@users.noreply.github.com>
2025-01-02 14:59:18 -0300

    updated FX Manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,15 @@
-
+fx_version("cerulean")
+game("gta5")
+author "JericoFX"
+description("JK-BloodPressure")
+version("1.0.0")
+client_script("client/init.lua")
+shared_script "@ox_lib/init.lua"
+server_scripts({
+    "config/sv_init.lua",
+	"server/init.lua",
+})
+files({
+	"config/cl_init.lua",
+})
+lua54("yes")

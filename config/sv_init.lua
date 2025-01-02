734c202666e5a467b932c6a7eea220c6afb26b18
JericoFX <blackjordison@gmail.com>
2025-01-02 20:16:36 -0300

    fix index

--- config/sv_init.lua
+++ config/sv_init.lua
@@ -1,18 +1,18 @@
 return {
     -- Normal Values
-    systolic = 120, 
+    systolic = 120,
     diastolic = 80,
     ---#region LOW BLOOD PRESSURE
-    lowItems = { -- Name and amount of % of the items that low the blood pressure 
+    lowItems = { -- Name and amount of % of the items that low the blood pressure
         beer = 3,
         vodka = 5,
         wine = 2,
     },
-    hiItems = { -- Name and amount of % of the items that increase the blood pressure 
+    hiItems = { -- Name and amount of % of the items that increase the blood pressure
         chocolate = 2,
-        "sugaritems" = 1,
-        "caffe" = 1,
-        "blablabla" = 1
+        sugaritems = 1,
+        caffe = 1,
+        blablabla = 1
     },
 
 }
